d926fc93fde5321e5ad718fcd98f947da5ea91eb
Emrik Östling <emrik.ostling@gmail.com>
2024-09-30 22:06:40 +0200

    add docker

--- app/page.tsx
+++ app/page.tsx
@@ -71,15 +71,16 @@ export default function Home() {
         <Link href={"/signup"} className="mb-20">
           <div
             className={`
-            relative flex flex-row rounded bg-slate-600 px-20 py-4 transition-colors
-            hover:bg-slate-500
-          `}
+              relative flex flex-row rounded bg-slate-600 px-20 py-4 transition-colors
+              hover:bg-slate-500
+            `}
           >
             Get Started
             <div
               className={`
-              absolute right-6 size-4 translate-y-1 rotate-45 border-r-2 border-t-2 border-slate-200
-            `}
+                absolute right-6 size-4 translate-y-1 rotate-45 border-r-2 border-t-2
+                border-slate-200
+              `}
             />
           </div>
         </Link>
