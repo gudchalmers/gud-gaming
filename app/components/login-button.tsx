2bd3f31948c12ea9a87bd7dd7b38af3ed2543f34
C4illin <emrik.ostling@gmail.com>
2024-12-16 16:42:03 +0100

    change auth

--- app/components/login-button.tsx
+++ app/components/login-button.tsx
@@ -3,11 +3,6 @@
 import { signIn } from "next-auth/react";
 
 export function LoginButton() {
-  const tenant = "chalmers.se";
-  const product = "GUD Gaming";
-  // const tenant = "boxyhq.com";
-  // const product = "saml-demo.boxyhq.com";
-
   return (
     <button
       className={`
@@ -17,11 +12,10 @@ export function LoginButton() {
       onClick={async (event) => {
         event.preventDefault();
         signIn(
-          "boxyhq-saml",
+          "MicrosoftEntraID",
           {
             callbackUrl: "/games",
           },
-          { tenant, product },
         );
       }}
     >
