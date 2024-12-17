e48e61da54b25cffb5f6ff068716514de4072fc0
C4illin <emrik.ostling@gmail.com>
2024-12-17 17:54:04 +0100

    fix redirect

--- app/components/login-button.tsx
+++ app/components/login-button.tsx
@@ -6,7 +6,7 @@ export function LoginButton() {
       action={async () => {
         "use server";
         await signIn("microsoft-entra-id", {
-          callbackUrl: "/games",
+          redirectTo: "/games",
         });
       }}
     >
--- app/components/logout-button.tsx
+++ app/components/logout-button.tsx
@@ -2,25 +2,28 @@ import { signOut } from "@/auth";
 
 export function LogoutButton() {
   return (
-    <button
-      className={`
-        relative flex flex-row rounded bg-slate-700 px-20 py-4 transition-colors
-        hover:bg-slate-600
-      `}
-      onClick={async (event) => {
+    <form
+      action={async () => {
         "use server";
-        event.preventDefault();
         await signOut({
           redirectTo: "/",
         });
       }}
     >
-      Logout
-      <div
+      <button
         className={`
-          absolute right-6 size-4 translate-y-1 rotate-45 border-r-2 border-t-2 border-slate-200
+          relative flex flex-row rounded bg-slate-700 px-20 py-4 transition-colors
+          hover:bg-slate-600
         `}
-      />
-    </button>
+        type="submit"
+      >
+        Logout
+        <div
+          className={`
+            absolute right-6 size-4 translate-y-1 rotate-45 border-r-2 border-t-2 border-slate-200
+          `}
+        />
+      </button>
+    </form>
   );
 }
